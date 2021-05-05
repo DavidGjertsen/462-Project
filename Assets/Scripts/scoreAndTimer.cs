75e23eff07f5daff7a67c472501f3708dada2828
David Gjertsen <davidgjertsen8635@gmail.com>
2021-05-05 15:26:29 -0400

    Added more GameTM

--- Assets/Scripts/scoreAndTimer.cs
+++ Assets/Scripts/scoreAndTimer.cs
@@ -1,15 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class scoreAndTimer : MonoBehaviour
 {
     public int score = 0;
-    //public int time = 60;
+    public int time = 90;
+
+    public Text scoreText;
+    public Text timerText;
+    public bool gameOver = false;
+
+    public GameObject UI;
+    public Text finalScore;
 
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        UI.SetActive(false);
+        StartCoroutine(Countdown());
+    }
+
+    IEnumerator Countdown() {
+        while(time > 0) {
+            yield return new WaitForSeconds(1);
+            time--;
+        }
+        gameOver = true;
+        GameOver();
+        Debug.Log("Game Over!");
+    }
+
+    void Update() {
+        scoreText.text = "$" + score;
+        timerText.text = "Steal as much as you can!\n\nBut be quick, the police will\n be here in " + time + " seconds!";
+    }
+
+    void GameOver() {
+        UI.SetActive(true);
+        finalScore.text = "$" + score;
+
+        Invoke("Restart", 10);
+    }
+
+    void Restart() {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
 }
--- Assets/Scripts/valuableObject.cs
+++ Assets/Scripts/valuableObject.cs
@@ -7,12 +7,17 @@ public class valuableObject : MonoBehaviour
     public int pointValue = 10;
     private bool hasBeenCounted = false;
     public scoreAndTimer gc;
+    public GameObject bagParticles;
+
+    void Start() {
+        bagParticles = GameObject.Find("BagParticles");
+    }
 
     public void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag == "bag" && hasBeenCounted == false) {
+        if(other.gameObject.tag == "bag" && hasBeenCounted == false && gc.gameOver == false) {
             hasBeenCounted = true;
+            bagParticles.GetComponent<ParticleSystem>().Play();
             gc.score += pointValue;
-            Debug.Log(gc.score);
         }
     }
 }
